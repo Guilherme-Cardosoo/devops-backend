da92c5778a0f56cca764ada62ad8d77239aa84cd
guilherme.cardosoo <gui04cardoso@gmail.com>
2026-07-05 03:05:31 -0300

    feat: adiciona configuração CORS para integração com frontend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.models import Produto
 from app.routes import router
 
-# Cria todas as tabelas do banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -13,8 +13,18 @@ app = FastAPI(
     description="API para gerenciamento de produtos."
 )
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(router)
 
 @app.get("/")
 def home():
