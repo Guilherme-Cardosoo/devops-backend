21d0e99006c970ac8939d67de3b5f63763f35083
guilherme.cardosoo <gui04cardoso@gmail.com>
2026-07-05 03:40:55 -0300

    config: atualiza CORS para frontend em Docker

--- app/main.py
+++ app/main.py
@@ -18,6 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
